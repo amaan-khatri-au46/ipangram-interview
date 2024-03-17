96975286d0d2d3641bb6bb54b3678e566bbbddab
amaankhatri <kurehiamaan91@gmail.com>
2024-03-17 22:58:22 +0530

    access denied img

--- Front-End/src/views/pages/AccessDenied/AccessDenied.tsx
+++ Front-End/src/views/pages/AccessDenied/AccessDenied.tsx
@@ -4,7 +4,7 @@ const AccessDenied = () => {
   return (
     <Container className="flex justify-center" style={{ height: "100vh" }}>
       <div className="h-full flex flex-col items-center justify-center">
-        <img src="/img/others/img-2.png" alt="Access Denied" />
+        <img src="/img/accessDenied/img-2-dark.png" alt="Access Denied" />
         <div className="mt-6 text-center">
           <h3 className="mb-2">Access Denied!</h3>
           <p className="text-base">You have no permission to visit this page</p>
